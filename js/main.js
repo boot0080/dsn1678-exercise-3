af8339c11da0c5464f3163ba8ae2cc2d5b944582
Ariah Boots <boot0080@algonquinlive.com>
2014-09-30 11:05:55 -0400

    added js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var salesDetails = $('.sales__details');
+var sales = $('.sales');
+
+
+var buttonClickHandler = function () {
+	var isActive = salesDetails.attr('data-state');
+	
+	if (isActive == 'active') {
+		salesDetails.attr('data-state', 'inactive');
+	} else {
+		salesDetails.attr('data-state', 'active');
+	}
+};	
+
+sales.on('click', buttonClickHandler);
